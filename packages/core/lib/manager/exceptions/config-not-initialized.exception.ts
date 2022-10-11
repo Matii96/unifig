86b48d7166ba79a75755ade0a791271277d7c521
Matii96 <mateusz.fonfara96@gmail.com>
2022-10-11 19:58:43 +0200

    feat(core): throwing exceptions upon encountering problems

---
+++ packages/core/lib/manager/exceptions/config-not-initialized.exception.ts
@@ -0,0 +1,8 @@
+import { Type } from '../../utils/type.interface';
+import { UnifigException } from '../../exceptions/unifig.exception';
+
+export class ConfigNotInitializedException extends UnifigException {
+  constructor(template: Type<any>) {
+    super(`Config template ${template.name} hasn't been initialized. Did you miss to register it?`);
+  }
+}
