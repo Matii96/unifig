fea793ea4b094005d426b847afb74bf0b3889618
Matii96 <mateusz.fonfara96@gmail.com>
2022-10-22 09:28:00 +0200

    fix(nest): ensured no templates names overlap
    injection tokens for config containers are now differentiated by additional id

--- packages/nest/lib/injection/get-config-container-token.factory.ts
+++ packages/nest/lib/injection/get-config-container-token.factory.ts
@@ -1,3 +1,16 @@
 import { Type } from '@nestjs/common';
 
-export const getConfigContainerToken = (template?: Type<any>) => `Unifig_container_${template?.name ?? 'GLOBAL'}`;
+let globalIdCounter = 0;
+const TEMPLATE_ID = '__unifigNestTemplateId__';
+
+export const getConfigContainerToken = (template?: Type) => {
+  if (!template) {
+    return 'Unifig_container_GLOBAL';
+  }
+  let id: number = Reflect.getMetadata(TEMPLATE_ID, template);
+  if (id === undefined) {
+    id = globalIdCounter++;
+    Reflect.defineMetadata(TEMPLATE_ID, id, template);
+  }
+  return `Unifig_container_${template.name}${id}`;
+};
