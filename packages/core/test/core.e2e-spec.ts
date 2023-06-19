f30862acb6483feead32a59f1978dd61c4e830e3
Matii96 <mateusz.fonfara96@gmail.com>
2023-06-20 00:33:50 +0200

    test(core): utilized functions adapters in e2e tests

--- packages/core/test/core.e2e-spec.ts
+++ packages/core/test/core.e2e-spec.ts
@@ -53,7 +53,7 @@ describe('@unifig/core (e2e)', () => {
     it('should convert port type', async () => {
       await manager.register({
         template: TransformationTemplate,
-        adapter: new PlainConfigAdapter({ local: { port: '3000' } }),
+        adapter: () => ({ local: { port: '3000' } }),
       });
       expect(manager.getValues(TransformationTemplate).port).toBe(3000);
     });
@@ -62,7 +62,7 @@ describe('@unifig/core (e2e)', () => {
       await manager.register({
         template: TransformationTemplate,
         enableImplicitConversion: false,
-        adapter: new PlainConfigAdapter({ local: { port: '3000' } }),
+        adapter: () => ({ local: { port: '3000' } }),
       });
       expect(manager.getValues(TransformationTemplate).port).toBe('3000');
     });
@@ -70,7 +70,7 @@ describe('@unifig/core (e2e)', () => {
     it('should leave missing properties blank', async () => {
       await manager.register({
         template: TransformationTemplate,
-        adapter: new PlainConfigAdapter({ local: { host: 'localhost' } }),
+        adapter: () => ({ local: { host: 'localhost' } }),
       });
       expect(manager.getValues(TransformationTemplate).port).not.toBeDefined();
     });
@@ -79,7 +79,7 @@ describe('@unifig/core (e2e)', () => {
       const ports = [{ port: 3000 }];
       await manager.register({
         template: TransformationArrayTemplate,
-        adapter: new PlainConfigAdapter({ ports } satisfies TransformationArrayTemplate),
+        adapter: () => ({ ports } satisfies TransformationArrayTemplate),
       });
       expect(manager.getValues(TransformationArrayTemplate).ports).toEqual(ports);
     });
@@ -89,7 +89,7 @@ describe('@unifig/core (e2e)', () => {
     it('should accept config values', async () => {
       await manager.register({
         template: ValidationTemplate,
-        adapter: new PlainConfigAdapter({ port: 3000, db: { url: 'db://localhost:5467' } }),
+        adapter: () => ({ port: 3000, db: { url: 'db://localhost:5467' } }),
       });
       expect(manager.getValues(ValidationTemplate)).toBeDefined();
     });
@@ -98,7 +98,7 @@ describe('@unifig/core (e2e)', () => {
       expect(
         manager.registerOrReject({
           template: ValidationTemplate,
-          adapter: new PlainConfigAdapter({ port: 3000, db: { port: 5000 } }),
+          adapter: () => ({ port: 3000, db: { port: 5000 } }),
         })
       ).rejects.toThrow(ConfigValidationError);
     });
